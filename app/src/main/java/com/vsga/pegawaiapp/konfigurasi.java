74998ac644269aef8cae8197a3ceca508b7c1562
Raditya Wijaya <radityawijaya021@gmail.com>
2021-08-18 21:50:29 +0800

    pegawaiApp

---
+++ app/src/main/java/com/vsga/pegawaiapp/konfigurasi.java
@@ -0,0 +1,11 @@
+package com.vsga.pegawaiapp;
+
+public class konfigurasi {
+    public static String BASE_URL="http://127.0.0.1/pegawai";
+    public  static final String URL_GET_ALL=BASE_URL+"tampilsemuapgw.php";
+    public static final String URL_ADD=BASE_URL+"tambahpgw.php";
+
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID="id";
+    public static final String TAG_NAME="name";
+}
